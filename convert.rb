ad79085a18b217b1761d31865677f948e021a6e6
galvdogs <47905704+galvdogs@users.noreply.github.com>
2019-03-17 16:01:55 +1100

    Update convert.rb

--- convert.rb
+++ convert.rb
@@ -1,26 +1,28 @@
-
+#! /usr/bin/env ruby
 
 #Prompt user whether to delete original files after successful conversion
 loop do
   print "Do you want original files to be deleted after conversion (y/n)? "
   dec_del = gets.chomp
+  print "\n"
   case dec_del
     when 'N', 'n'
       break
     when 'Y', 'y'
       print "Original files will be deleted. Are you sure (y/n)? "
       dec_del_confirm = gets.chomp
+      print "\n"
       case dec_del_confirm
         when 'Y', 'y'
           break
         when 'N', 'n'
           dec_del = ''
         else
-          print "Input invalid. Try again or Ctrl + C to cancel this process"
-          dec_del = '' 
+          puts "Input invalid. Try again or Ctrl + C to cancel this process. "
+          dec_del = ''
       end
     else
-      print "Input invalid. Try again or Ctrl + C to cancel this process"
+      puts "Input invalid. Try again or Ctrl + C to cancel this process. "
       dec_del = ''
   end
 end
@@ -29,6 +31,11 @@ end
 loop do
   print "Type the path where the videos are located (all videos at this path will be converted): "
   src_path = gets.chomp
+  unless File.directory?(src_path)
+    "File path is not valid, or permission denied. Please try again."
+    src_path = ""
+  else
+    break
 end
   
     
